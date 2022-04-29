5e8ed345aeb7f1c2b45d7ffc48b7981818f0e96c
Dayara Rossi <100045697+dayaracardoso@users.noreply.github.com>
2022-04-29 16:31:22 -0300

    Enviando arquivos

---
+++ script.js
@@ -0,0 +1,16 @@
+﻿var display = document.getElementById("display"); // define o display
+
+function inserir(num) { // pega o valor do button clicado
+  display.value += num; // adiciona sempre o que for clicado ao que já existe no display
+}
+
+function limpaCalc() { // define o valor do display como vazio
+  display.value = "";
+}
+
+function calcular() {
+  temConta = display.value;
+  if(temConta) {
+    display.value = eval(temConta);
+  }
+}
